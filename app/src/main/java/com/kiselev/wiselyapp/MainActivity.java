c7468bc92891c7abe65c18e5af6aedb3e774eace
Aaaaa988 <kiselev-v.s.99@yandex.ru>
2021-04-16 14:52:41 +0700

    add spend and income activity

--- app/src/main/java/com/kiselev/wiselyapp/MainActivity.java
+++ app/src/main/java/com/kiselev/wiselyapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.kiselev.wiselyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_sp_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+        button_sp_in = (Button)findViewById(R.id.button_spend_income);
+
+        button_sp_in.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.kiselev.wiselyapp.SpendIncomeActivity");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
